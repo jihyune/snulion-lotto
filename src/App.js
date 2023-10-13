c9b1280950dbb97f93b6c89c1c1bc9ed03292b2e
jihyune <rlawlcns96@gmail.com>
2023-10-13 22:17:06 +0900

    feat: get winner prize

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
-import { useConnect, useAccount } from "wagmi";
+import { useConnect, useAccount, useBalance } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { disconnect } from "@wagmi/core";
 import { useFaucet } from "./hooks/faucet";
 import { useEnter } from "./hooks/enter";
 import { useApprove } from "./hooks/approve";
+import { LOTTO_CONTRACT_ADDRESS, TOKEN_CONTRACT_ADDRESS } from "./constant";
+import { useAllowance } from "./hooks/allowance";
 
 function App() {
-  const reward = 10;
-  const participants = ["lsjfdlakfs", "asfjlasjdfl", "alsfjlsjd"];
-
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   });
@@ -17,18 +16,25 @@ function App() {
   const handleConnect = () => {
     isConnected ? disconnect() : connect();
   };
-
   const { write: faucet } = useFaucet();
-  const { write: enter } = useEnter();
-  const { write: approve, allowance } = useApprove();
 
+  const allowance = useAllowance();
+  const { write: approve } = useApprove();
+  const { write: enter } = useEnter();
   const handleEnter = () => {
-    if (!allowance) {
+    // eslint-disable-next-line no-undef
+    if (Number(BigInt(allowance ?? "0")) === 0) {
       approve();
       return;
     }
     enter();
   };
+
+  const { data } = useBalance({
+    address: LOTTO_CONTRACT_ADDRESS,
+    token: TOKEN_CONTRACT_ADDRESS,
+  });
+  const reward = data?.formatted;
 
   return (
     <div className="App">
@@ -44,19 +50,9 @@ function App() {
       </div>
       <div>
         <div className="title">Jack Pot ! {reward} LION 🪙</div>
-        <div className="sub-title">
-          Congraturations! You are the winner! claim {reward} LION!
-        </div>
       </div>
 
       <button onClick={handleEnter}>Enter</button>
-
-      <div>
-        <div>Participants</div>
-        {participants.map((participant) => {
-          return <div>{participant}</div>;
-        })}
-      </div>
     </div>
   );
 }
