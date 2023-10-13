3bc95f74648b0d8cf838c4a9dd6e9b475cadd8c7
jihyune <rlawlcns96@gmail.com>
2023-10-13 21:54:21 +0900

    feat: allowance and approve

---
+++ src/hooks/allowance.js
@@ -0,0 +1,17 @@
+import { LOTTO_CONTRACT_ADDRESS, TOKEN_CONTRACT_ADDRESS } from "../constant";
+
+import { useContractRead, useAccount } from "wagmi";
+import { TOKEN_ABI } from "../abi/token";
+
+export const useAllowance = () => {
+  const { address: walletAddress } = useAccount();
+
+  const { data } = useContractRead({
+    address: TOKEN_CONTRACT_ADDRESS,
+    abi: TOKEN_ABI,
+    functionName: "allowance",
+    args: [walletAddress, LOTTO_CONTRACT_ADDRESS],
+  });
+
+  return data;
+};
--- src/hooks/approve.js
+++ src/hooks/approve.js
@@ -1,32 +1,9 @@
 import { LOTTO_CONTRACT_ADDRESS, TOKEN_CONTRACT_ADDRESS } from "../constant";
 
-import {
-  useContractRead,
-  useContractWrite,
-  usePrepareContractWrite,
-  useAccount,
-} from "wagmi";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
 import { TOKEN_ABI } from "../abi/token";
-import { useState } from "react";
 
 export const useApprove = () => {
-  const [allowance, setAllowance] = useState(false);
-  const { address: walletAddress } = useAccount();
-
-  useContractRead({
-    address: TOKEN_CONTRACT_ADDRESS,
-    abi: TOKEN_ABI,
-    functionName: "allowance",
-    args: [walletAddress, LOTTO_CONTRACT_ADDRESS],
-    enabled: !!walletAddress,
-    onSuccess: (data) => {
-      // eslint-disable-next-line no-undef
-      const bigInt = BigInt(data?.toString() ?? "0");
-
-      setAllowance(bigInt > 0);
-    },
-  });
-
   const { config } = usePrepareContractWrite({
     address: TOKEN_CONTRACT_ADDRESS,
     abi: TOKEN_ABI,
@@ -35,9 +12,8 @@ export const useApprove = () => {
       LOTTO_CONTRACT_ADDRESS,
       "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
     ],
-    enabled: !allowance,
   });
   const { write } = useContractWrite(config);
 
-  return { write, allowance };
+  return { write };
 };
